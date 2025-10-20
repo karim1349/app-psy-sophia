d3aaa7a61c14e2a4ff9348db080834fa6f6f642a
Karim Benchekroun <karimbenchekroun@outlook.fr>
2025-10-20 21:37:43 +0100

    static file fix

--- apps/api/app-psy-sophia/settings.py
+++ apps/api/app-psy-sophia/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # R2 (S3-compatible)
